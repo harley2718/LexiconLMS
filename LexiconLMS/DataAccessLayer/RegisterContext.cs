f4cfad0fccb5d96c72ed71cd631c281ce766e6a6
lenaholmstrom <User@DESK-GDZ7J32>
2018-03-07 16:48:13 +0100

    Adding context

---
+++ LexiconLMS/DataAccessLayer/RegisterContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using LexiconLMS.Models;
+
+namespace LexiconLMS.DataAccessLayer
+{
+    public class RegisterContext : DbContext
+    {
+
+        public RegisterContext() : base("LexiconLMS")
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Module> Modules { get; set; }
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<Activity> Activity { get; set; }
+        public DbSet<ActivityType> ActivitytTypes { get; set; }
+    }
+
+    
+}
---
+++ LexiconLMS/Models/Document.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LexiconLMS.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public int DocumentId { get; set; }
+        public int ReferredDocId { get; set; }
+        public DateTime ResponseDeadline { get; set; }
+        public string Contents { get; set; }
+    }
+}
