22313f2c50b02d207f3f0507f543a14236710883
lenaholmstrom <User@DESK-GDZ7J32>
2018-03-08 09:45:12 +0100

    dbConext etc

--- LexiconLMS/Models/IdentityModels.cs
+++ LexiconLMS/Models/IdentityModels.cs
@@ -1,22 +1,9 @@
 ﻿using System.Data.Entity;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using LexiconLMS.Models;
 
 namespace LexiconLMS.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
-    {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
-        {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
-            return userIdentity;
-        }
-    }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
@@ -29,5 +16,14 @@ namespace LexiconLMS.Models
         {
             return new ApplicationDbContext();
         }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Module> Modules { get; set; }
+        //public DbSet<Document> Documents { get; set; }
+        public DbSet<Activity> Activity { get; set; }
+        public DbSet<ActivityType> ActivitytTypes { get; set; }
     }
+
+    
+
 }
