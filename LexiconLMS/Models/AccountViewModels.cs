ad789d24626e72aed59c8d96e31a315eb790542d
lenaholmstrom <User@DESK-GDZ7J32>
2018-03-12 13:29:21 +0100

    Changes layout login page

--- LexiconLMS/Models/AccountViewModels.cs
+++ LexiconLMS/Models/AccountViewModels.cs
@@ -55,10 +55,10 @@ namespace LexiconLMS.Models
 
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Lösenord")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "Kom ihåg mig?")]
         public bool RememberMe { get; set; }
     }
 
@@ -70,14 +70,14 @@ namespace LexiconLMS.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "Lösenordet {0} måste vara minst {2} tecken långt.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Lösenord")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Bekräfta lösenord")]
+        [Compare("Lösenord", ErrorMessage = "Lösenorden stämmer inte överens.")]
         public string ConfirmPassword { get; set; }
     }
 
