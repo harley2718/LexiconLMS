83a78faf27f174feb165f6b106a7d34e5198ce68
Hans Davidsson <error@nodomain.local>
2018-03-13 16:55:57 +0100

    UserViewModel updated and one UserController Create method changed.

--- LexiconLMS/Models/ViewModels/UserViewModel.cs
+++ LexiconLMS/Models/ViewModels/UserViewModel.cs
@@ -9,10 +9,17 @@ using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace LexiconLMS.Models.ViewModels
 {
-
-        public class UserViewModel 
+    public class UserViewModel
     {
         public int Id { get; set; }
+
+#if true
+        public int     CourseId   { get; set; }  // Each students should be
+        public string  CourseName { get; set; }  // associated with a course.
+        public Boolean IsTeacher  { get; set; }  // A couple of booleans can
+        public Boolean IsStudent  { get; set; }  // be used to control variants
+                                                 // of a view.
+#endif
 
         [Required(ErrorMessage = "Fältet får inte vara tomt.")]
         [StringLength(20, ErrorMessage = "Fältet år inte innehålla mer än 20 tecken.")]
@@ -42,4 +49,4 @@ namespace LexiconLMS.Models.ViewModels
         [DisplayName("Mobil")]
         public string UserPhoneNumber { get; set; }
     }
- }
+}
