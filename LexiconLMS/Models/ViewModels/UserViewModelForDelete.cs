c0c27b80c4eae9c1848ace1f095ade23930efab2
Hans Davidsson <error@nodomain.local>
2018-03-23 14:12:55 +0100

    Added a view model:  Models/ViewModels/UserViewModelForDelete.cs

---
+++ LexiconLMS/Models/ViewModels/UserViewModelForDelete.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LexiconLMS.Models.ViewModels
+{
+    public class UserViewModelForDelete
+    {
+        public UserViewModel User       { get; set; }
+        public string        CourseName { get; set; }
+    }
+}
+
